8c6a00954b16091e4742f066f433b1d3841999cd
Pierre <pierre.girolet@gmail.com>
2015-04-09 10:03:33 +0200

    e ne sais pas

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,9 @@
+#include <QKeyEvent>
+#include <QMessageBox>
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <iostream>
+#include <stdio.h>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -13,4 +16,15 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *event)
+{
+    switch (event->key()) {
+    case Qt::Key_Up:
+        QMessageBox::warning(this, "titre messageBox", "la touche up a ete activee");
+        break;
+    default:
+        QWidget::keyPressEvent(event);
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -14,6 +14,7 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    void keyPressEvent(QKeyEvent *event);
 
 private:
     Ui::MainWindow *ui;
